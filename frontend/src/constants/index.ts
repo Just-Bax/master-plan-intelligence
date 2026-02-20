eb51308035f18c10f59cd46906719101e6b0a55a
Just-Bax <isfandiyor.baxtiyorov.uz@gmail.com>
2026-02-21 00:36:17 +0500

    Add dist

--- frontend/src/constants/index.ts
+++ frontend/src/constants/index.ts
@@ -1,7 +1,4 @@
-const API_BASE_URL =
-  typeof import.meta.env.VITE_API_BASE_URL === "string"
-    ? import.meta.env.VITE_API_BASE_URL
-    : "http://localhost:8000";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const AUTH_TOKEN_KEY = "access_token";
 
